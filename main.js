07e666dce78809e2b32f52ab6e6bfd7fa5ec234c
dcthoai <dcthoai1023@gmail.com>
2023-10-28 20:28:24 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+// turn on list suggest for input tag
+function showSearchbarList(){
+    var element = document.getElementById("searchbar__listbox");
+    element.style.display = "block";
+}
+
+function hiddenSearchbarList(){
+    var element = document.getElementById("searchbar__listbox");
+    element.style.display = "none";
+}
